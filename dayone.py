5c058f87ee6b237e64cd800a93f9a8b971d13c35
tareqkb24-meet <t32185278@gmail.com>
2024-07-14 17:16:55 +0300

    first lab

--- dayone.py
+++ dayone.py
@@ -1,1 +1,14 @@
-print("dayone")
+import random
+tempetures = []
+days_of_the_week= ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturdy']
+int good_days_count = 0
+for i in range(1,8):
+	int t = random.randit(27,41)
+    int num = t 
+    if num % 2 == 0:
+    	good_days_count = good_days_count + 1
+    
+
+
+
+
