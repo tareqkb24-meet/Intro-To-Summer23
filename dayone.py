c6c98c829f39b2cb6c4de172a879b4d623c1330d
tareqkb24-meet <t32185278@gmail.com>
2024-07-15 00:02:46 +0300

    Update dayone.py

--- dayone.py
+++ dayone.py
@@ -1,13 +1,46 @@
+
+
 import random
-tempetures = []
-days_of_the_week= ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturdy']
-int good_days_count = 0
-for i in range(1,8):
-	int t = random.randit(27,41)
-    int num = t 
-    if num % 2 == 0:
-    	good_days_count = good_days_count + 1
-    
+high_temp = 0
+lower_temp = 40
+alltemp = 0
+aboveavg= []
+good_days_counter=0
+temperatures = []
+daytemp = []
+days_of_the_week=["Sunday","Monday","Tuesday","wednesday","Thursday","friday","Saturday"]
+for i in range (7):
+  temperatures.append(random.randint(26,40))
+  if temperatures[i]%2==0:
+    good_days_counter=good_days_counter+1
+    daytemp.append(days_of_the_week[i])
+  alltemp = alltemp+temperatures[i]
+  print(days_of_the_week[i],":",temperatures[i])
+avgtemp = alltemp / 7
+highest_temp_day=""
+lowest_temp_day = " "
+for i in range (7):
+  if high_temp<temperatures[i]:
+    highest_temp=temperatures[i]
+    highest_temp_day=days_of_the_week[i]
+  if lower_temp>temperatures[i]:
+    lowest_temp=temperatures[i]
+    lowest_temp_day=days_of_the_week[i]
+
+  if avgtemp<temperatures[i]:
+    aboveavg.append(days_of_the_week[i])
+
+
+
+
+
+print("shelly had ",good_days_counter,"good days")
+print("the avrage temp is :",avgtemp)
+print("above avg days were: ", aboveavg)
+print("highest temp is: ",high_temp,"on ",highest_temp_day)
+print("lowest temp is: ",lower_temp, "on ", lowest_temp_day)
+
+
 
 
 
